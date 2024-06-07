3cfda30690f1fc31c2cebc93047204d80aed2941
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-07 15:34:35 +0530

    Prisma, Prisma adapter

--- lib/env.ts
+++ lib/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    AUTH_SECRET: z.string(),
     DATABASE_URL: z.string().url(),
     AUTH_GOOGLE_ID: z.string(),
     AUTH_GOOGLE_SECRET: z.string(),
